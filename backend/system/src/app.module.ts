50877e632a544d4184169c03b08203b45797fe95
minjun-P <jun7332568@gmail.com>
2024-05-10 16:25:27 +0900

    feat : registry, 등록과 resolver -> godidy 사용해서 동작하게 세팅

--- backend/system/src/app.module.ts
+++ backend/system/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IssuerModule } from './issuer/issuer.module';
-import { JwtService } from './jwt/jwt.service';
+import { RegistryModule } from './registry/registry.module';
+import { RegistryController } from './registry/registry.controller';
+import { RegistryService } from './registry/registry.service';
 
 @Module({
-  imports: [IssuerModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [IssuerModule, RegistryModule],
+  controllers: [AppController, RegistryController],
+  providers: [AppService, RegistryService],
 })
 export class AppModule {}
---
+++ backend/system/src/core/godid_api_key.ts
@@ -0,0 +1,1 @@
+export const firstGoDidKey = '1dab0a13-f73b-425e-899d-fe2ec27666c3';
