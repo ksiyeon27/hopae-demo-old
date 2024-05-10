203bdf9ed96856902621d33d9e790c919d76a61c
PrE-Ren <xhdtlf99@naver.com>
2024-05-10 14:31:59 +0900

    mainpage1 desc

--- frontend/issuer/pages/index.tsx
+++ frontend/issuer/pages/index.tsx
@@ -3,7 +3,7 @@ import { v4 } from "uuid";
 
 function MainPage() {
   const useDeepLink = () => {
-    const url = "https://dummy.com";
+    const url = "https://dummy.com"; // issuer 1 backend
     const uuid = v4();
     window.open("wwwallet://issue?url=" + url + "&randomString=" + uuid);
   };
@@ -15,8 +15,8 @@ function MainPage() {
         justifyContent: "center",
       }}
     >
-      <h1>Issuer Main Page</h1>
-      <Button onClick={useDeepLink}>{"버튼"}</Button>
+      <h1>Issuer 1 Main Page</h1>
+      <Button onClick={useDeepLink}>{"인증서 발급하기"}</Button>
     </Box>
   );
 }
