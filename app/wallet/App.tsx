6a41295ed1feee6ba4f80d5038b0955d756bee4a
PrE-Ren <xhdtlf99@naver.com>
2024-06-15 19:39:14 +0900

    remove unused

--- app/wallet/App.tsx
+++ app/wallet/App.tsx
@@ -16,7 +16,6 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import HomeScreen from '@/screens/home';
 import VerifyScreen from '@/screens/verify';
 import IssueScreen from '@/screens/issue';
-import PolyfillCrypto from 'react-native-webview-crypto';
 
 const config = {
   screens: {
