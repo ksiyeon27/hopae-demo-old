5f045a1aff46bd0ad795487ba4bfa80218cecbf5
minjun-P <jun7332568@gmail.com>
2024-06-19 11:21:29 +0900

    feat: resolver module, env에 따라 web, dock 갖다 쓰도록

--- backend/system/src/did_resolver/did_resolver.module.ts
+++ backend/system/src/did_resolver/did_resolver.module.ts
@@ -1,8 +1,34 @@
 import { Module } from '@nestjs/common';
-import { DidResolverService } from './did_resolver.service';
+import {
+  DidResolverService,
+  DidResolverServiceDockImpl,
+  DidResolverServiceWebImpl,
+} from './did_resolver.service';
+import { DidResolverController } from './did_resolver.controller';
+import { DockService } from 'src/dock/dock.service';
+import { DockModule } from 'src/dock/dock.module';
+import { DockDidUtilService } from 'src/dock/util_service/util.service';
+
+const didResolverService = {
+  provide: DidResolverService,
+  useFactory: (
+    dockService: DockService,
+    dockDidUtilService: DockDidUtilService,
+  ) => {
+    console.log('use factory -------------');
+    console.log(process.env.DOCK);
+    console.log(process.env.DOCK === 'true');
+    return process.env.DOCK === 'true'
+      ? new DidResolverServiceDockImpl(dockService, dockDidUtilService)
+      : new DidResolverServiceWebImpl();
+  },
+  inject: [DockService, DockDidUtilService],
+};
 
 @Module({
-  providers: [DidResolverService],
-  exports: [DidResolverService],
+  imports: [DockModule],
+  providers: [didResolverService, DockService, DockDidUtilService],
+  controllers: [DidResolverController],
+  exports: [didResolverService],
 })
 export class DidResolverModule {}
--- backend/system/src/did_resolver/did_resolver.service.ts
+++ backend/system/src/did_resolver/did_resolver.service.ts
@@ -1,18 +1,63 @@
 import { HttpException, Injectable } from '@nestjs/common';
+import { DockService } from 'src/dock/dock.service';
+import { DockDidUtilService } from 'src/dock/util_service/util.service';
 
 @Injectable()
 export class DidResolverService {
+  getDidDoc(did: string): Promise<Record<string, any>> {
+    throw new Error('Method not implemented.');
+  }
+
+  getPulbicKeyByDid(did: string): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class DidResolverServiceWebImpl implements DidResolverService {
+  async getPulbicKeyByDid(did: string): Promise<string> {
+    const didDoc = await this.getDidDoc(did);
+    if (!didDoc) {
+      return undefined;
+    }
+    const verificationMethods = didDoc.verificationMethod;
+    // 걍 첫번째꺼 가져오자
+    const publicKeyJwk = verificationMethods[0].publicKeyJwk;
+    return publicKeyJwk;
+  }
   async getDidDoc(did: string) {
     console.log(`==DidResolverService: getDidDoc ${did} ==`);
+    console.log(process.env.DOCK);
     const res = await fetch(`https://web-did-registry.vercel.app/did/${did}`, {
       method: 'GET',
     });
-    if (res.status !== 200) {
-      throw new HttpException('해당하는 did가 web-registry에 없음', 400);
-    }
     const didDoc = await res.json();
     // console.log(didDoc);
+    if (!res.ok) {
+      return undefined;
+    }
+    return didDoc;
+  }
+}
 
+export class DidResolverServiceDockImpl implements DidResolverService {
+  constructor(
+    readonly dockService: DockService,
+    readonly dockDidUtilService: DockDidUtilService,
+  ) {}
+  async getPulbicKeyByDid(did: string): Promise<any> {
+    const didDoc = await this.getDidDoc(did);
+    const publicKeysOfDoc = didDoc.publicKey;
+    const base58EncodedKeys = publicKeysOfDoc.map(
+      (key: any) => key.publicKeyBase58,
+    );
+    const pulblicJwk =
+      this.dockDidUtilService.aggregateBase58KeysToJwk(base58EncodedKeys);
+    return pulblicJwk;
+  }
+  async getDidDoc(did: string): Promise<Record<string, any>> {
+    await this.dockService.connectToNode();
+    const didDoc = await this.dockService.resolveDid(did);
+    await this.dockService.disconnectNode();
     return didDoc;
   }
 }
