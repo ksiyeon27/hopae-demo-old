ca8b73dcdceb4d0bb25b38e03323a5832835f48f
PrE-Ren <xhdtlf99@naver.com>
2024-06-19 11:55:38 +0900

    hosting

--- frontend/web/common/config.ts
+++ frontend/web/common/config.ts
@@ -1,3 +1,3 @@
 export const backendBaseURL = "http://localhost:8000";
-export const backendHostingURL = "https://e717-121-170-212-116.ngrok-free.app";
+export const backendHostingURL = "https://76b5-222-112-23-245.ngrok-free.app";
 export const issuerDid = "issuer_did_1";
--- frontend/web/pages/issuer1/issuance/index.tsx
+++ frontend/web/pages/issuer1/issuance/index.tsx
@@ -1,19 +1,8 @@
-import { Box, Button } from "@mui/material";
-import Component from './component';
-import Layout from './layout';
+import Component from "./component";
 import { FC } from "react";
-import { backendHostingURL } from "@/common/config";
 
 const Issuer1Page: FC = () => {
-  const useDeepLink = () => {
-    const url = backendHostingURL + "/issuer/vc/career"; // issuer 1 backend
-    const nonceUrl = backendHostingURL + "/issuer/nonce/career"; // 대신 서버에서 가져와야함
-    window.open("wwwallet://issue?url=" + url + "&nonceUrl=" + nonceUrl);
-  };
-
-  return (
-    <Component />
-  );
+  return <Component />;
 };
 
 export default Issuer1Page;
